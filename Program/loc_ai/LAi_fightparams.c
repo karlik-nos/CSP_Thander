ff97611d591640bce6f73a25876e777d75acf6a3
Anthony Lapenok <anton.lapenok@gmail.com>
2022-01-08 04:26:29 +0300

    Update LAi_fightparams.c

--- Program/loc_ai/LAi_fightparams.c
+++ Program/loc_ai/LAi_fightparams.c
@@ -330,7 +330,7 @@ float LAi_CalcDamageForBlade(aref attack, aref enemy, string attackType, bool is
 		
 		//Boyer mod #20170318-33 difficulty level rebalancing
 		//if (MOD_SKILL_ENEMY_RATE < 5 && sti(enemy.index) == GetMainCharacterIndex())
-		if (MOD_SKILL_ENEMY_RATE < 10 && sti(enemy.index) == GetMainCharacterIndex())
+		if (MOD_SKILL_ENEMY_RATE < 11 && sti(enemy.index) == GetMainCharacterIndex())
 		{
 			dmg = dmg * (4.0 + MOD_SKILL_ENEMY_RATE) / 10.0;
 		}
