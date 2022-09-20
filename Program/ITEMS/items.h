79a459168740f9f62760e2b704ebe8c014abc95a
Sinistraa <Sireusss@yandex.ru>
2022-09-20 23:02:52 +0300

    конфликты решение

--- Program/ITEMS/items.h
+++ Program/ITEMS/items.h
@@ -36,7 +36,6 @@
 #define IDOLS_RIGHT_ITEM_TYPE 				"idols_right"
 
 
-#define TOTAL_ITEMS				1000 // Warship 08.05.09 - Новая система предметов
 #define ITEMS_QUANTITY			  422  // не забываем менять номер
 
 #define MAX_BUTTONS               64
