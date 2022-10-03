4b243bd4a291fc41cd1b638f87f1a68595da2cfd
xxxLEOPARDxxx <riperblazer@narod.ru>
2022-10-04 02:06:03 +0300

    Update _mod_on_off.h

--- Program/_mod_on_off.h
+++ Program/_mod_on_off.h
@@ -2,7 +2,7 @@
 // здесь и далее ник boal - это разработческий ник ALexusB
 
 #define VERSION_NUMBER1       "ГПК v.1.3.2 ATR  "
-#define VERSION_NUMBER2       "CSP v2.3.0a (03.10.2022)"
+#define VERSION_NUMBER2       "CSP v2.3.0a (04.10.2022)"
 #define VERSION_NUMBER3       ""
 #define PROVIDER_TXT            "LEOPARD's team"
 #define VERSION_NUM      99910  // текущий номер
