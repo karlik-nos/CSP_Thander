068a35fce63454f6095eee0bd4ff6d8c9ce6aa23
xxxLEOPARDxxx <riperblazer@narod.ru>
2022-10-08 02:09:26 +0300

    Revert "Фикс пушек"
    This reverts commit 25996d04563b15b51153be7e2db9bb5f5f5801ac.

--- Program/SEA_AI/AICannon.c
+++ Program/SEA_AI/AICannon.c
@@ -285,10 +285,6 @@ int GetBortIntactCannonsNum(ref rCharacter, string sBort, int iNumCannonsOnBort)
 	}
 
 	int iNumIntactCannons = 0;
-	if (arDamages=="null") 
-	{
-		return 0;
-	}
 	for (int i=0; i<iNumCannonsOnBort; i++)
 	{
 		fDamage = stf(GetAttributeValue(GetAttributeN(arDamages, i)));
