4ec202513a246a4c2a74727fdfd4003d1fb94b20
RetrowaveMan <warrior250798@mail.ru>
2022-10-12 04:39:14 +0700

    Фикс пустых диалогов контрабандистов после патруля

--- Program/DIALOGS/russian/Smuggler_OnShore_dialog.c
+++ Program/DIALOGS/russian/Smuggler_OnShore_dialog.c
@@ -188,7 +188,7 @@ void ProcessDialogEvent()
 							dialog.Text = RandPhraseSimple("Спасибо за помощь. Мы не забудем этого. Отправляемся.", "Молодец! Отлично сражал"+ GetSexPhrase("ся","ась") +". Идем.");
 							ChangeContrabandRelation(PChar, 5);
 							DeleteAttribute(PChar,"GenQuest.contraTravel.PatrolFight");
-							DeleteAttribute(NPChar,"ContrabandInterruption");
+							ContrabandInterruptionSetStatus(false);
 							AddCharacterExpToSkill(Pchar, "Sneak", 50);
 							Link.l2 = "Уже иду.";
 							Link.l2.go = "Exit";
@@ -387,10 +387,7 @@ void ProcessDialogEvent()
             if(GetSummonSkillFromName(pchar, "Sneak") < Rand(120))
 			{
 				AddDialogExitQuest("Rand_ContrabandInterruption");
-				for (i=1; i<=3+makeint(MOD_SKILL_ENEMY_RATE/2); i++)
-				{
-					characters[GetCharacterIndex("Rand_Smug0"+i)].ContrabandInterruption = true;
-				}
+				ContrabandInterruptionSetStatus(true);
 			}
 			AddCharacterExpToSkill(Pchar, "Sneak", 100);
 			Diag.CurrentNode = Diag.TempNode;
