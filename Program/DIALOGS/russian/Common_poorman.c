c7b15d1d2d6dcc0b88d867c5efcfa3e851cf6c2d
Qwerry, St <amihaliss@gmail.com>
2022-11-08 20:09:09 +0300

    Update Common_poorman.c

--- Program/DIALOGS/russian/Common_poorman.c
+++ Program/DIALOGS/russian/Common_poorman.c
@@ -135,7 +135,7 @@ void ProcessDialogEvent()
 						link.l1.go = "FoundLetter";
 					}
 					//базар о том, что нашёл причину заказа на нищих
-					if (pchar.questTemp.LSC == "readyGoLSC")
+					if (pchar.questTemp.LSC == "readyGoLSC" && GetCharacterFreeItem(pchar, "keyQuestLSC")==0)
 					{
 						link.l1 = "Ты знаешь, мне удалось распутать этот клубок!";
 						link.l1.go = "GoLSC";
