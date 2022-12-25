43835d57506d1de550ab91782163a21ba286e98d
Qwerry, St <amihaliss@gmail.com>
2022-12-25 12:31:28 +0300

    фикс продажи в ГПК больше, чем привёз (не тестировалось)

--- Program/DIALOGS/russian/Quest/LostShipsCity/Priest.c
+++ Program/DIALOGS/russian/Quest/LostShipsCity/Priest.c
@@ -552,6 +552,8 @@ void ProcessDialogEvent()
 		break;
 		case "TradeMed2":
 			npchar.amount = GetStrSmallRegister(dialogEditStrings[1]);
+			npchar.amount = func_min((sti(npchar.amount),sti(pchar.Ship.Cargo.Goods.Medicament));//фикс, нельзя продать больше, чем привёз
+			npchar.amount = func_max((sti(npchar.amount),1);
 			NPChar.sumtotal = sti(npchar.amount)*10;
 			//dialog.text = "Значит, что получается? Я беру лекарства по 10 за ед. Итого, "+NPChar.sumtotal+" золотых за "+npchar.amount+" ед. За каждые 15 тысяч идёт кредитный сундук. Предложение устраивает?";
 			dialog.text = "Значит, что получается? Я беру лекарства по 10 за ед. Итого, "+NPChar.sumtotal+" золотых за "+npchar.amount+" ед. Предложение устраивает?";
@@ -586,6 +588,8 @@ void ProcessDialogEvent()
 		break;
 		case "TradeCloth2":
 			npchar.amount = GetStrSmallRegister(dialogEditStrings[1]);
+			npchar.amount = func_min((sti(npchar.amount),sti(pchar.Ship.Cargo.Goods.Clothes));//фикс, нельзя продать больше, чем привёз
+			npchar.amount = func_max((sti(npchar.amount),1);
 			NPChar.sumtotal = sti(npchar.amount)*5;
 			//dialog.text = "Значит, что получается? Я беру одежду по 5 за ед. Итого, "+NPChar.sumtotal+" золотых за "+npchar.amount+" ед. За каждые 15 тысяч идёт кредитный сундук. Предложение устраивает?";
 			dialog.text = "Значит, что получается? Я беру одежду по 5 за ед. Итого, "+NPChar.sumtotal+" золотых за "+npchar.amount+" ед. Предложение устраивает?";
