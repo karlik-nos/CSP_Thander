905c1ea15541ec1dba6da01926e7127960640b36
Qwerry, St <amihaliss@gmail.com>
2023-01-16 20:32:55 +0300

    лишний лог удалил

--- Program/DIALOGS/russian/Quest/ForAll_dialog.c
+++ Program/DIALOGS/russian/Quest/ForAll_dialog.c
@@ -229,7 +229,6 @@ void ProcessDialogEvent()
 		break;
 		case "GiveKeyMan_7":
 			npchar.quest.price = makeint((6666 * ((sti(pchar.rank))/2.0)*MOD_SKILL_ENEMY_RATE*(100-(GetCharacterSkill(pchar, SKILL_LEADERSHIP)/2)))/100);
-			Log_Info(""+npchar.quest.price);
 			if (sti(pchar.money) >= sti(npchar.quest.price))
 			{
 				dialog.text = "М-м... хорошо! Я хочу за этот ключ " + FindRussianMoneyString(sti(npchar.quest.price)) + ".";
@@ -252,7 +251,6 @@ void ProcessDialogEvent()
 		break;
 		case "GiveKeyMan_NoMoney":
 			npchar.quest.price = makeint((6666 * ((sti(pchar.rank))/2.0)*MOD_SKILL_ENEMY_RATE*(100-(GetCharacterSkill(pchar, SKILL_LEADERSHIP)/2)))/100);
-			Log_Info(""+npchar.quest.price);
 			if (sti(pchar.money) >= sti(npchar.quest.price))
 			{
 				dialog.text = "Ну вот, уже лучше, как говорится... Давай вернёмся к нашему разговору о ключе. В общем, я готов его тебе сделать, предположим, за " + FindRussianMoneyString(sti(npchar.quest.price)) + ".";
