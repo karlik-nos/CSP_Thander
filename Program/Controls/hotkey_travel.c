1ffa3965f90bbb74aa2abd94140dea13c971b0ea
Anthony Lapenok <anton.lapenok@gmail.com>
2023-01-25 18:26:19 +0300

    Enable fast reload to port without ship
    Теперь можно тпшится в порт, если корабль не в нём

--- Program/Controls/hotkey_travel.c
+++ Program/Controls/hotkey_travel.c
@@ -15,7 +15,6 @@ void HKT_Button(string sHKB) // быстрый переход
 	if(!IsEnableFastTravel()) bOk = false;
 	if (chrDisableReloadToLocation) bOk = false;
 	if (sHKB != "Fast_port" && !CheckFastJump(Locations[curLocIdx].id, sCityID + locID)) bOk = false;
-	if (sHKB == "Fast_port" && !CheckFastJump(Locations[curLocIdx].id, pchar.location.from_sea)) bOk = false;
 	if (!bBettaTestMode && bOk) // проверка города на враждебность
 	{
 		string sNation = Colonies[FindColony(loadedLocation.fastreload)].nation;
@@ -37,7 +36,7 @@ void HKT_Button(string sHKB) // быстрый переход
 	if(bOk)
 	{
 		if (sHKB == "Fast_port")
-			PlayerFastTravel(curLocIdx, pchar.location.from_sea, "reload1");
+			PlayerFastTravel(curLocIdx, sCityID + "_town", "reload1");
 			else PlayerFastTravel(curLocIdx, sCityID + locID, "");
 	}
 	else
@@ -46,11 +45,6 @@ void HKT_Button(string sHKB) // быстрый переход
 		PlaySound("interface\knock.wav");
 	}
 	if(pchar.location == sCityID + locID)
-	{
-		Log_info(XI_ConvertString("You are already there"));
-		PlaySound("interface\knock.wav");
-	}
-	if (sHKB == "Fast_port" && pchar.location == pchar.location.from_sea)
 	{
 		Log_info(XI_ConvertString("You are already there"));
 		PlaySound("interface\knock.wav");
